258721afd0997d3e625edbc8f99a14f9776f0ef8
ny-01 <ny-01@github.com>
2022-05-26 00:01:48 +0100

    Overloaded the constructor so as to allow an initial configuration of the grid to be set. Implemented a method to apply the rules to a given Grid object and in so doing produce the next generation, which is immediately printed to the terminal when the method is called.

--- Grid.java
+++ Grid.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Grid {
     static final int GRID_WIDTH = 30;
     static final int GRID_HEIGHT = 30;
@@ -15,7 +17,7 @@ public class Grid {
         for (int i = 0; i < GRID_WIDTH + 2; i++) {
             System.out.println();
             for (int j = 0; j < GRID_HEIGHT + 2; j++) {
-                if (i > 0 && j > 0 && i < 31 && j < 31) {
+                if (i > 0 && j > 0 && i < (GRID_WIDTH + 1) && j < (GRID_HEIGHT + 1)) {
                     if (!gridState[i - 1][j - 1]) {
                         System.out.print(DEAD_CHARACTER);
                     }
@@ -30,12 +32,71 @@ public class Grid {
         }
     }
 
-    // set start config
-    public void setGridState(boolean[][] gridState) {
-        this.gridState = gridState;
+    Grid(ArrayList<Integer[]> startConfig) {
+        for (int i = 0; i < GRID_WIDTH; i++) {
+            for (int j = 0; j < GRID_HEIGHT; j++) {
+                gridState[i][j] = false;
+            }
+        }
+
+        for (Integer[] coordinate : startConfig) {
+            gridState[coordinate[0]][coordinate[1]] = true;
+        }
+
+        for (int i = 0; i < GRID_WIDTH + 2; i++) {
+            System.out.println();
+            for (int j = 0; j < GRID_HEIGHT + 2; j++) {
+                if (i > 0 && j > 0 && i < (GRID_WIDTH + 1) && j < (GRID_HEIGHT + 1)) {
+                    if (!gridState[i - 1][j - 1]) {
+                        System.out.print(DEAD_CHARACTER);
+                    }
+                    else{
+                        System.out.print(ALIVE_CHARACTER);
+                    }
+                }
+                else {
+                    System.out.print("~");
+                }
+            }
+        }
+    }
+
+    public Grid updateGrid() {
+        ArrayList<Integer[]> config = new ArrayList<>();
+        for (int i = 1; i < (GRID_WIDTH - 1); i++) {
+            for (int j = 1; j < (GRID_HEIGHT - 1); j++) {
+                // Apply the rules of the game
+                int aliveCount = 0;
+                for (int x = -1; x <= 1; x++) {
+                    for (int y = -1; y <= 1; y++) {
+                        if (gridState[i+x][j+y] && !((x == 0) && (y == 0))) {
+                            aliveCount++;
+                        }
+                    }
+                }
+                if ((aliveCount == 2 && (gridState[i][j])) || (aliveCount == 3)) {
+                    Integer[] array = {i, j};
+                    config.add(array);
+                }
+            }
+        }
+        Grid newGrid = new Grid(config);
+        return newGrid;
     }
 
     public static void main(String[] args) {
-        Grid grid = new Grid();
+        ArrayList<Integer[]> start = new ArrayList<>();
+        Integer[] array = {15, 15};
+        Integer[] array1 = {16, 16};
+        Integer[] array2 = {17, 15};
+        Integer[] array3 = {17, 16};
+        Integer[] array4 = {16,17};
+        start.add(array);
+        start.add(array1);
+        start.add(array2);
+        start.add(array3);
+        start.add(array4);
+        Grid grid = new Grid(start);
+        grid.updateGrid();
     }
 }
